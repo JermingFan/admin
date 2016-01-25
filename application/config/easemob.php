c32508213097d5989e1dd9bab2bf5aa768d10716
JermingFan <jermingfan@foxmail.com>
2016-01-25 09:43:43 +0800

    update

---
+++ application/config/easemob.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ *
+ * Easemob PaaS
+ * http://docs.easemob.com/docs.php
+ *
+ */
+
+// Easemob Client Id
+$config['client_id'] = 'XXX';
+
+// Easemob Client Secret
+$config['client_secret'] = 'XXX';
+
+// Easemob Org Name
+$config['org_name'] = 'XXX';
+
+// Easemob App Name
+$config['app_name'] = 'XXX';
